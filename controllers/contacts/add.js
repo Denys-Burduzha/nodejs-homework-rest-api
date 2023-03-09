32d33e9f3d3762355a9287b1c7a9de9bcaff4eb9
Denys-Burduzha <106137997+Denys-Burduzha@users.noreply.github.com>
2023-03-09 17:39:11 +0100

    HW-03-mongodb

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,11 +1,10 @@
 
 
-const contactsOperations = require("../../models/contacts");
-
+const {Contact} = require("../../models");
 
 
 const add = async(req, res) => {
-        const result = await contactsOperations.addContacts(req.body)
+        const result = await Contact.create(req.body);
         res.status(201).json({
             status: "success",
             code: 201,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,21 +2,22 @@
 const express = require('express')
 
 const {validation, ctrlWrapper} = require("../../middlrwares");
-const contactsSchema = require("../../schemas");
+const {joiSchema, favoriteJoiSchema} = require("../../models/contact");
 const {contacts: ctrl} = require("../../controllers");
 
 
-const validateMiddleware = validation(contactsSchema);
 const router = express.Router()
 
 router.get('/', ctrlWrapper(ctrl.getAll));
 
 router.get('/:id', ctrlWrapper(ctrl.getById));
 
-router.post('/', validateMiddleware, ctrlWrapper(ctrl.add));
+router.post('/', validation(joiSchema), ctrlWrapper(ctrl.add));
+
+router.put('/:id', validation(joiSchema), ctrlWrapper(ctrl.updateById));
+
+router.patch('/:id/favorite', validation(favoriteJoiSchema), ctrlWrapper(ctrl.updateFavorite));
 
 router.delete('/:id', ctrlWrapper(ctrl.removeById));
 
-router.put('/:id', validateMiddleware, ctrlWrapper(ctrl.apdateById));
-
-module.exports = router
+module.exports = router;
